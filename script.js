c25fa30eaddc4db6dae225c8e438febcd2556716
Danna González <dannagonzz707k.t@gmail.com>
2024-06-24 21:33:12 -0600

    Actualización de Portafolio

---
+++ script.js
@@ -0,0 +1,12 @@
+/*  
+
+    Archivo de JS que contiene un script (conjunto de lineas de código que normalmente buscan realizar alguna acción en específico).
+
+*/
+
+const edad = 25;
+
+alert('Conectarse')
+
+
+alert(edad)
