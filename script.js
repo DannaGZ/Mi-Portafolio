e8e4a63cfe4734a2fc7167221e1e72f1aa147ff6
Danna González <dannagonzz707k.t@gmail.com>
2024-06-24 22:29:12 -0600

    Portafolio Final

--- script.js
+++ script.js
@@ -8,5 +8,23 @@ const edad = 25;
 
 alert('Conectarse')
 
-
 alert(edad)
+
+let
+ app = document.getElementById('app');
+
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+
+typewriter
+  .pauseFor(500)
+  .typeString('Hola, soy estudiante de Administración y Marketing')
+  .pauseFor(500)
+  .deleteChars(50)
+  .typeString('Hola, me gusta escuchar electro-swing')
+  .deleteChars(50)
+  .typeString('Hola, me encantan los gatitos')
+  .pauseFor(500)
+  .start();
